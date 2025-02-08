48909718ea14e74537916ece588606ba78f94204
Anastasiia <anastasiiahrokholska@gmail.com>
2025-02-08 17:49:55 +0200

    Added another request if query is empty

--- app/src/main/java/com/ahrokholska/gifs/data/network/GifService.kt
+++ app/src/main/java/com/ahrokholska/gifs/data/network/GifService.kt
@@ -12,4 +12,11 @@ interface GifService {
         @Query("q") query: String,
         @Query("bundle") bundle: String = "messaging_non_clips",
     ): GifResponse
+
+    @GET("/v1/gifs/trending")
+    suspend fun getTrendingGifs(
+        @Query("limit") limit: Int,
+        @Query("offset") offset: Int,
+        @Query("bundle") bundle: String = "messaging_non_clips",
+    ): GifResponse
 }
